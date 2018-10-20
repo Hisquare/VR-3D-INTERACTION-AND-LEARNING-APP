86f4e62e1ccb531fa33e5f5272fbae34eb20423a
Uchechukwu Obimma <philshisquare@gmail.com>
2018-10-20 10:45:53 +0100

    THIS SCRIPT CONTROLS THE 3D MENU
    It enables the user drag the menu around

---
+++ DragZone.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragZone : gazableObjects {
+
+    private VRcanvas parentPanel;
+    private Transform originalParent;
+    private InputMode savedInputMode = InputMode.NONE;
+	// Use this for initialization
+	void Start () {
+        parentPanel = GetComponentInParent<VRcanvas>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public override void OnPress(RaycastHit hitInfo)
+    {
+        base.OnPress(hitInfo);
+
+        // make the entire canvas a child of the camera to move it
+        originalParent = parentPanel.transform.parent;
+        parentPanel.transform.parent = Camera.main.transform;
+
+        // save the old input mode and set the current mode to drag
+        savedInputMode = player.instance.activeMode;
+        player.instance.activeMode = InputMode.DRAG;
+    }
+
+    public override void OnRelease(RaycastHit hitInfo)
+    {
+        base.OnRelease(hitInfo);
+
+        // reply old values
+        parentPanel.transform.parent = originalParent;
+        player.instance.activeMode = savedInputMode;
+    }
+
+}
